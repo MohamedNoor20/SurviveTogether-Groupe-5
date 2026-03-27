aeccbbf3efa0f681ed31a8d59e7ee90daece84f3
SusanOgozi <susanaogozi21@gmail.com>
2026-03-27 21:42:51 +0000

    Add Hazard touch detection logic

--- src/griffith/Hazard.java
+++ src/griffith/Hazard.java
@@ -1,5 +1,6 @@
 package griffith;
 import java.awt.Rectangle; 
+
 public class Hazard {
 	 public Rectangle area;
 	 public Type type;
@@ -7,5 +8,11 @@ public class Hazard {
 		this.area = area;
 		this.type = type;
 	}
-
+public void check(Player p) {
+	 Rectangle playerBox = new Rectangle(p.x, p.y, 40, 40);
+	 if (area.intersects(playerBox)) {
+         System.out.println("TOUCH!");
 }
+}
+}
+
