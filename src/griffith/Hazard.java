5c9b2e5c7ba78213854d8425373e42a466b17cc4
SusanOgozi <susanaogozi21@gmail.com>
2026-03-27 20:56:46 +0000

    Add fields and constructor with empty hazard fire or water pools

--- src/griffith/Hazard.java
+++ src/griffith/Hazard.java
@@ -1,9 +1,11 @@
 package griffith;
-
+import java.awt.Rectangle; 
 public class Hazard {
-
-	public Hazard() {
-		
+	 public Rectangle area;
+	 public Type type;
+	public Hazard(Rectangle area, Type type) {
+		this.area = area;
+		this.type = type;
 	}
 
 }
