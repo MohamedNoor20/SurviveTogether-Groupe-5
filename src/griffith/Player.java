a64bc0474ce1524c7bcb15e018b1538f2bb6fba2
SusanOgozi <susanaogozi21@gmail.com>
2026-03-27 23:32:33 +0000

    Add temporary class with constructor

--- src/griffith/Player.java
+++ src/griffith/Player.java
@@ -1,17 +1,10 @@
 package griffith;
 
-/**
- * TEMP Player for Person 4 tests
- * Person 2 will replace with real one
- */
+
 public class Player {
-    public int x, y;
-    public Type type;
-    public boolean alive = true;
     
-    public Player(int x, int y, Type type) {
-        this.x = x;
-        this.y = y;
-        this.type = type;
+    
+    public Player() {
+        
     }
 }
