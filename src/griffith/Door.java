514cf3ca8fd712151e7cc4403ed0b16d0cb06c12
SusanOgozi <susanaogozi21@gmail.com>
2026-03-27 22:13:30 +0000

    Add Door.isInside()  for win check

--- src/griffith/Door.java
+++ src/griffith/Door.java
@@ -8,4 +8,7 @@ public class Door { //
     public Door(Rectangle area) {
         this.area = area;
     }
+    public boolean isInside(Player p) {
+        return area.intersects(new Rectangle(p.x, p.y, 40, 40));
+}
 }
