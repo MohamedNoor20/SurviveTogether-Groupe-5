b2c504d94e39ac473cc1153c8abe0ba1a4a0248d
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-29 10:06:00 +0100

    this creates the character

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -1,5 +1,29 @@
-package griffith;
+import java.awt.Rectangle;
 
-public class GamePanel {
+import javax.swing.JPanel;
 
+public class GamePanel extends JPanel {
+    Player player1;
+    Player player2;
+
+    Hazard firePool;
+    Hazard waterPool;
+
+    Door door;
+
+    public GamePanel() {
+        setFocusable(true);
+
+        // Create players
+        player1 = new Player(50, 50, Type.FIRE);
+        player2 = new Player(50, 150, Type.WATER);
+
+        // Create hazards
+        firePool = new Hazard(new Rectangle(300, 100, 100, 50), Type.FIRE);
+        waterPool = new Hazard(new Rectangle(300, 100, 100, 50), Type.WATER);
+
+        // Create door
+        door = new Door(new Rectangle(600, 150, 50, 80));
+    }
+    
 }
