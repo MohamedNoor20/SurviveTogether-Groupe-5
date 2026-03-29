fbf6eb1a873664bba70836bfaa395a4406d2d0a7
afaqahmed <afaqahmedstdy@gmail.com>
2026-03-29 16:06:50 +0100

    added graphics stub methods in GamePanel

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -50,4 +50,49 @@ public class GamePanel extends JPanel {
         }
     }
     
+    
+    //Afaq Ahmed
+    //Graphics methods stubs
+
+     //this returns the fireboy X-axis position
+     public int getFireboyX() {
+         return 0;
+         }
+
+     //this returns the fireboy Y-axis position
+     public int getFireboyY() {
+         return 0;
+         }
+
+     ////this returns the watergirl Y-axis position
+     public int getWatergirlX() {
+         return 0;
+         }
+
+     ////this returns the watergirl Y-axis position
+     public int getWatergirlY() {
+         return 0;
+         }
+
+     //this will return if fireboy is alive
+     public boolean isFireboyAlive() {
+         return true;
+         }
+
+     //this will return if watergirl is alive
+     public boolean isWatergirlAlive() {
+         return true;
+         }
+
+     //this will return fireboy color
+     public java.awt.Color getFireboyColor() {
+         return java.awt.Color.RED;
+         }
+
+     //this will return watergirl color
+     public java.awt.Color getWatergirlColor() {
+         return java.awt.Color.BLUE;
+         }
+    
+    
 }
