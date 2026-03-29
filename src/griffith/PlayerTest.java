d935a60970159f2a650f34b549bb763838985bb8
MuadMSET <muad4404@gmail.com>
2026-03-29 22:59:35 +0100

    Update PlayerTest.java
    added the test methods

--- src/griffith/PlayerTest.java
+++ src/griffith/PlayerTest.java
@@ -1,5 +1,26 @@
 package griffith;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+
+    private Player player;
+    private int SCREEN_WIDTH = 800;
+    private int SCREEN_HEIGHT = 600;
+
+        @Test
+    public void testInitialPosition() {}
+
+    @Test
+    public void testNormalMovement() {}
+
+    @Test
+    public void testTopAndLeftBoundaries() {}
+
+    @Test
+    public void testBottomAndRightBoundaries() {}
     
 }
