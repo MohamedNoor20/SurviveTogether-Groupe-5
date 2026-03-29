2e1a51926c494318a393e0b2c4611d4548136f88
MuadMSET <muad4404@gmail.com>
2026-03-29 23:26:05 +0100

    Create PlayerTest.java
    added PlayerTest class again

---
+++ src/griffith/PlayerTest.java
@@ -0,0 +1,34 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void testMoveUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMoveDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMoveRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGravity() {
+		fail("Not yet implemented");
+	}
+
+}
