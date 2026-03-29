49e4b48192798d5021e24d84dcbbecc8f264617d
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-29 10:21:58 +0100

    created Main class

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -11,7 +11,7 @@ public class GamePanel extends JPanel {
     Hazard waterPool;
 
     Door door;
-
+    // Mohamed
     public GamePanel() {
         setFocusable(true);
 
@@ -25,6 +25,27 @@ public class GamePanel extends JPanel {
 
         // Create door
         door = new Door(new Rectangle(600, 150, 50, 80));
+    }
+    // Mohamed
+    // This runs the frame
+    public void updateGame() {
+
+        // This will call Susan's logic
+        firePool.check(player1);
+        firePool.check(player2);
+
+        waterPool.check(player1);
+        waterPool.check(player2);
+
+        // Win condition
+        if (door.isInside(player1) && door.isInside(player2)) {
+            System.out.println("YOU WIN!");
+        }
+
+        // Lose condition
+        if (!player1.alive || !player2.alive) {
+            System.out.println("GAME OVER");
+        }
     }
     
 }
