a31485e44a5db7d997ad409378f15f56770add7d
SusanOgozi <susanaogozi21@gmail.com>
2026-04-08 13:32:56 +0100

    Add constructor with fields

--- src/griffith/Diamond.java
+++ src/griffith/Diamond.java
@@ -4,4 +4,10 @@ public class Diamond {
 	public Type type;            
     public int x, y;              
     public boolean collected = false;
+    
+    public Diamond(Type type, int x, int y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+}
 }
