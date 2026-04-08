c996e33b3a5cc39e46bc9cc48b5c3de07045f482
SusanOgozi <susanaogozi21@gmail.com>
2026-04-08 11:52:09 +0100

    Add GREEN and ICE types to Type enum

--- src/griffith/Type.java
+++ src/griffith/Type.java
@@ -2,6 +2,6 @@ package griffith;
  
   //Player types for collision rules (susan ogozi)
 public enum Type {
-FIRE, WATER // this creates 2 fixed player types fire boy safe in fire and water girl type safe in water(susan ogozi)
-                        
+FIRE, WATER,// this creates 2 fixed player types fire boy safe in fire and water girl type safe in water(susan ogozi)
+GREEN, ICE             
 }
