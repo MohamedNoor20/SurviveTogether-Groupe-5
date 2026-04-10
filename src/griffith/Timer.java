4894de0f60b8191352a8a6e3a867866145e7e2e8
SusanOgozi <susanaogozi21@gmail.com>
2026-04-10 19:57:34 +0100

    Add start method to Timer

--- src/griffith/Timer.java
+++ src/griffith/Timer.java
@@ -4,7 +4,12 @@ public class Timer {
 private long timerstarts;
 public boolean isRunning = false;
 	public Timer() {
+		isRunning = false;
 		
+	}
+	public void start() {
+	    timerstarts = System.currentTimeMillis();
+	    isRunning = true;
 	}
 
 }
