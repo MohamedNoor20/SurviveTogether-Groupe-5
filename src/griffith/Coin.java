d2972d57a7d11701cb857edfc3498d646f6dc830
MuadMSET <muad4404@gmail.com>
2026-04-12 20:17:49 +0100

    Update Coin.java
    added the implementation of the method

--- src/griffith/Coin.java
+++ src/griffith/Coin.java
@@ -10,7 +10,10 @@ public class Coin {
 	}
 	
 	public void checkCollision (Player p) {
-
+		if (!isCollected && area.intersects(p.getBounds())) {
+			this.isCollected = true;
+			p.score++;
+		}
 	}
 
 }
