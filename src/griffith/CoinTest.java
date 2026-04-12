e493cb29a84fa222a9de0847681c5bd0785a3c92
MuadMSET <muad4404@gmail.com>
2026-04-12 22:00:18 +0100

    Update CoinTest.java
    added the implementation of testCoinCollectionStatus

--- src/griffith/CoinTest.java
+++ src/griffith/CoinTest.java
@@ -11,6 +11,14 @@ public class CoinTest {
 	
 	@Test
     void testCoinCollectionStatus() {
+		player = new Player(100, 100, Type.FIRE);
+		coin = new Coin(new Rectangle(100, 100, 20, 20)); 
+		// the coin should not be collected before the player touch it 
+		assertFalse(coin.isCollected, "Coin not collected.");
+		//now we will make the player touch the coin and it should change 
+		coin.checkCollision(player);
+        assertTrue(coin.isCollected, "Coin is collected.");
+		
 		
 	}
 }
