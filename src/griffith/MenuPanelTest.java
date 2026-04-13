29b04a70eef699297d341ad02347caf438016f69
afaqahmed <afaqahmedstdy@gmail.com>
2026-04-13 19:09:42 +0100

    added test class for menu

---
+++ src/griffith/MenuPanelTest.java
@@ -0,0 +1,14 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MenuPanelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
