9df578d46fc1f5f235bba6c82ba20f7af8ce87d2
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 10:04:54 +0100

    Add MainMenuPanel constructor

--- src/griffith/NewMainMenuPanel.java
+++ src/griffith/NewMainMenuPanel.java
@@ -13,4 +13,15 @@ public class NewMainMenuPanel extends JPanel {
 	    private JButton exitButton;
 	    private Main mainFrame;
 	    private Image backgroundImage;
+	    
+	    public NewMainMenuPanel(Main main) {
+	        this.mainFrame = main;
+	        
+	        double scale = (main != null) ? main.scale : 1.0;
+	        int width = (int)(768 * scale);
+	        int height = (int)(768 * scale);
+	        this.setPreferredSize(new Dimension(width, height));
+	        
+	        setLayout(null);
+}
 }
