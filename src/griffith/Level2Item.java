3d0e6d7df4ceb09f824198df0026ba6322d0f144
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 12:21:09 +0100

    Add imports and constants to Level2Item

--- src/griffith/Level2Item.java
+++ src/griffith/Level2Item.java
@@ -1,5 +1,15 @@
 package griffith;
 
-public class Level2Item {
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
+public class Level2Item {
+    
+    public static final int ITEM_WIDTH = 21;
+    public static final int ITEM_HEIGHT = 21;
+    private int state;
+    
+    private Image img;
+    private int x, y, w, h;
+    
 }
