78520f07f96deb82f6edd7dba977429fa18689ba
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 12:27:49 +0100

    Add imports and constants to Level2Door

--- src/griffith/Level2Door.java
+++ src/griffith/Level2Door.java
@@ -1,5 +1,15 @@
 package griffith;
 
-public class Level2Door {
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
+public class Level2Door {
+    
+    public static final int DOOR_WIDTH = 56;
+    public static final int DOOR_HEIGHT = 64;
+    private int state;
+    
+    private Image img;
+    private int x, y, w, h;
+    
 }
