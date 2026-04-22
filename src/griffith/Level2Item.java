ce93a127f936892e53c4328fe80ff8b9d8b23cdb
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 12:23:07 +0100

    Add setImage method to Level2Item

--- src/griffith/Level2Item.java
+++ src/griffith/Level2Item.java
@@ -25,4 +25,11 @@ public class Level2Item {
     private ImageIcon resizeImage(ImageIcon icon, int width, int height) {
         return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
     }
+    public void setImage() {
+        if (state % 2 == 0) {
+            img = resizeImage(new ImageIcon("src/static/image/elements/water_gem.png"), ITEM_WIDTH, ITEM_HEIGHT).getImage();
+        } else {
+            img = resizeImage(new ImageIcon("src/static/image/elements/fire_gem.png"), ITEM_WIDTH, ITEM_HEIGHT).getImage();
+        }
+    }
 }
