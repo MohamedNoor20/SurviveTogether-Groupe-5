745861873de98b1abf4e3166ff001b0420a3c0e8
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 10:13:57 +0100

    Add DifficultyPanel constructor and dark background

--- src/griffith/DifficultyPanel.java
+++ src/griffith/DifficultyPanel.java
@@ -10,4 +10,17 @@ public class DifficultyPanel {
     private JButton mediumButton;
     private JButton backButton;
     private Main mainFrame;
+    
+    
+    public DifficultyPanel(Main main) {
+        this.mainFrame = main;
+        
+        double scale = (main != null) ? main.scale : 1.0;
+        int width = (int)(768 * scale);
+        int height = (int)(768 * scale);
+        this.setPreferredSize(new Dimension(width, height));
+        
+        setLayout(null);
+        setBackground(new Color(18, 18, 38));
+}
 }
