d6b014fdd528bc33364b198ff276f65acacd51a0
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 10:01:00 +0100

    Create new main menu panel and add imports

---
+++ src/griffith/NewMainMenuPanel.java
@@ -0,0 +1,13 @@
+package griffith;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+public class NewMainMenuPanel {
+
+}
