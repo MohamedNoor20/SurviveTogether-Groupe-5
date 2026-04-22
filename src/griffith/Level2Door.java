ba828a0395b6db26de010886162b6bc26602898e
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 12:30:52 +0100

    Add getter methods to Level2Door for image, position, size, and state

--- src/griffith/Level2Door.java
+++ src/griffith/Level2Door.java
@@ -22,9 +22,11 @@ public class Level2Door {
         this.state = state;
         setImage();
     }
+    
     private ImageIcon resizeImage(ImageIcon icon, int width, int height) {
         return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
     }
+    
     public void setImage() {
         if (state % 2 == 0) {
             img = resizeImage(new ImageIcon("src/static/image/elements/water_door.png"), DOOR_WIDTH, DOOR_HEIGHT).getImage();
@@ -32,4 +34,11 @@ public class Level2Door {
             img = resizeImage(new ImageIcon("src/static/image/elements/fire_door.png"), DOOR_WIDTH, DOOR_HEIGHT).getImage();
         }
     }
+    
+    public Image getImage() { return img; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return w; }
+    public int getHeight() { return h; }
+    public int getState() { return state; }
 }
