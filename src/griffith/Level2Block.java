a10c579f696cbedfe732a7a1544e053de94b55fa
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 12:57:10 +0100

    created level 2 block with complete fields

--- src/griffith/Level2Block.java
+++ src/griffith/Level2Block.java
@@ -1,5 +1,35 @@
 package griffith;
 
-public class Level2Block {
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
 
+public class Level2Block {
+    
+    public static final int BLOCK_WIDTH = 31;
+    public static final int BLOCK_HEIGHT = 32;
+    
+    private Image img;
+    private Rectangle blockRect;
+    private int x, y, w, h;
+    
+    public Level2Block() {
+        img = new ImageIcon("src/static/image/elements/block.png").getImage();
+    }
+    
+    public Level2Block(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.w = BLOCK_WIDTH;
+        this.h = BLOCK_HEIGHT;
+        this.blockRect = new Rectangle(x, y, w, h);
+        img = new ImageIcon("src/static/image/elements/block.png").getImage();
+    }
+    
+    public Image getImage() { return img; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return w; }
+    public int getHeight() { return h; }
+    public Rectangle getRectangle() { return blockRect; }
 }
