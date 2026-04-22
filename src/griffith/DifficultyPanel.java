3633a89d8a326cd28d1285683e529a7785bc8881
SusanOgozi <susanaogozi21@gmail.com>
2026-04-22 10:11:50 +0100

    Create Difficulty selection class with imports

---
+++ src/griffith/DifficultyPanel.java
@@ -0,0 +1,10 @@
+package griffith;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+public class DifficultyPanel {
+
+}
