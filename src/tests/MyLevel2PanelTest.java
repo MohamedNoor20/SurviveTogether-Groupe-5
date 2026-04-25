8901ce55deca53981eb24a0c71070f55328276a5
SusanOgozi <susanaogozi21@gmail.com>
2026-04-25 09:16:58 +0100

    Create new class and add package declaration and imports for JUnit test class

--- src/tests/MyLevel2PanelTest.java
+++ src/tests/MyLevel2PanelTest.java
@@ -1,5 +1,12 @@
 package tests;
-
+//susan Ogozi
+//3257092
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import java.awt.Rectangle;
+//Test all junit test for my level panel
 public class MyLevel2PanelTest {
 
+    
 }
