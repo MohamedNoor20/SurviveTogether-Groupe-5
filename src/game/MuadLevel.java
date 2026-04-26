17fafcdc5cabe71954119e4617bba75716685874
MuadMSET <muad4404@gmail.com>
2026-04-26 23:37:17 +0100

    Update MuadLevel.java
    added a player position + floor to test

--- src/game/MuadLevel.java
+++ src/game/MuadLevel.java
@@ -8,6 +8,25 @@ public class MuadLevel extends Level{
 
 
 public MuadLevel() {
+    p1StartX = 50;
+    p1StartY = 650;
+    p2StartX = 120;
+    p2StartY = 650;
+    
+    
+    floors.add(new Floor(new Rectangle(0, 715, 768, 40))); 
+    
+
+    floors.add(new Floor(new Rectangle(0, 550, 300, 20))); 
+    
+
+    floors.add(new Floor(new Rectangle(400, 400, 368, 20))); 
+    
+
+    floors.add(new Floor(new Rectangle(0, 250, 300, 20))); 
+    
+
+    floors.add(new Floor(new Rectangle(400, 160, 368, 20))); 
 
 	}
 }
