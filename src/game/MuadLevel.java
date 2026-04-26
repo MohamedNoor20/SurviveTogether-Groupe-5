f3e3763b6051eefa5334c6a13de46af696cb9e73
MuadMSET <muad4404@gmail.com>
2026-04-26 23:37:31 +0100

    Update MuadLevel.java

--- src/game/MuadLevel.java
+++ src/game/MuadLevel.java
@@ -15,18 +15,6 @@ public MuadLevel() {
     
     
     floors.add(new Floor(new Rectangle(0, 715, 768, 40))); 
-    
-
-    floors.add(new Floor(new Rectangle(0, 550, 300, 20))); 
-    
-
-    floors.add(new Floor(new Rectangle(400, 400, 368, 20))); 
-    
-
-    floors.add(new Floor(new Rectangle(0, 250, 300, 20))); 
-    
-
-    floors.add(new Floor(new Rectangle(400, 160, 368, 20))); 
 
 	}
 }
