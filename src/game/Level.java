ff6b2aa5251583b4240c48540ac88002437bcc6e
MuadMSET <muad4404@gmail.com>
2026-04-26 23:33:03 +0100

    Update Level.java
    added the player starting points variables + the floor array list

--- src/game/Level.java
+++ src/game/Level.java
@@ -1,5 +1,16 @@
 package game;
 
+import entities.*;
+import java.util.ArrayList;
+
 public class Level {
+	
+	//player starting point
+	public int p1StartX, p1StartY;
+    public int p2StartX, p2StartY;
+    
+    public Door door;
+    
+    public ArrayList<Floor> floors = new ArrayList<>();
 
 }
