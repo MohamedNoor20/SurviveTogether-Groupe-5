2d236a03d9696df52d817e12804b208233f2c528
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-04-26 19:44:58 +0100

    Update BottomTest.java

--- src/tests/BottomTest.java
+++ src/tests/BottomTest.java
@@ -3,21 +3,18 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import entities.Bottom;
-import entities.Player;
-import entities.Type;
+import entities.*;
 
 import java.awt.Rectangle;
 
 public class BottomTest {
 
-    @Test
-    public void testPressTrue() {
-        Bottom bottom = new Bottom(new Rectangle(100, 100, 50, 50));
-        Player player1 = new Player(110, 110, Type.FIRE);
-        Player player2 = new Player(110, 110, Type.FIRE);
+	@Test
+	public void testPressTrue() {
+		Bottom bottom = new Bottom(new Rectangle(100, 100, 50, 50));
+		Player player1 = new Player(110, 110, Type.FIRE);
+		Player player2 = new Player(110, 110, Type.FIRE);
 
-        assertTrue(bottom.press(player1) || bottom.press(player2));
-    }
-
+		assertTrue(bottom.press(player1) || bottom.press(player2));
+	}
 }
