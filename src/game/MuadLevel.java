bd425005c4ba7d2520da06e1702e8268f0eac3e6
MuadMSET <muad4404@gmail.com>
2026-04-27 18:04:06 +0100

    Update MuadLevel.java
    designed my leveeeeeeeeeeeeeel

--- src/game/MuadLevel.java
+++ src/game/MuadLevel.java
@@ -8,43 +8,31 @@ public class MuadLevel extends Level{
 
 public MuadLevel() {
     // 1. PLAYER SPAWNS (Bottom Left)
-    p1StartX = 50; 
+	p1StartX = 50; 
     p1StartY = 650;
     p2StartX = 120; 
     p2StartY = 650;
 
-
-    door = new Door(new Rectangle(640,680, 60, 80));
-
- 
-    bottom = new Bottom(new Rectangle(100, 100, 80, 10)); 
-    firePool = new Hazard(new Rectangle(100, 100, 80, 10), Type.FIRE);
-    waterPool = new Hazard(new Rectangle(100, 100, 80, 10), Type.WATER);
-    greenPool = new Hazard(new Rectangle(100, 100, 80, 10), Type.GREEN);
-    iceFloor.add(new Floor(new Rectangle(150, 715, 490, 10)));
-    
-    floors.add(new Floor(new Rectangle(0, 715, 768, 40)));
-
-
-    floors.add(new Floor(new Rectangle(0, 550, 300, 20)));
-
-
-    floors.add(new Floor(new Rectangle(350, 420, 250, 20)));
-
-
-    floors.add(new Floor(new Rectangle(50, 280, 200, 20)));
-
-
-    floors.add(new Floor(new Rectangle(450, 160, 318, 20)));
-    
-
-    coins.add(new Coin(new Rectangle(150, 510, 20, 20)));
-    coins.add(new Coin(new Rectangle(450, 380, 20, 20)));
-    portal.add(new Portal(new Rectangle(600, 655, 40, 60), 450, 100));
-    bouncePad.add(new BouncePad(new Rectangle(350, 700, 50, 15)));
-
-
-	}
+    door = new Door(new Rectangle(650, 70, 60, 80));
+    floors.add(new Floor(new Rectangle(0, 715, 300, 40)));
+    floors.add(new Floor(new Rectangle(468, 715, 300, 40)));
+    floors.add(new Floor(new Rectangle(0, 500, 200, 20)));
+    floors.add(new Floor(new Rectangle(0, 280, 300, 20)));
+    floors.add(new Floor(new Rectangle(450, 150, 318, 20)));
+    iceFloor.add(new Floor(new Rectangle(250, 500, 400, 10)));
+    greenPool = new Hazard(new Rectangle(300, 700, 168, 10), Type.GREEN);
+    firePool = new Hazard(new Rectangle(300, 473, 80, 7), Type.FIRE);
+    waterPool = new Hazard(new Rectangle(450, 473, 80, 7), Type.WATER);
+    bouncePad.add(new BouncePad(new Rectangle(200, 700, 50, 15)));
+    bouncePad.add(new BouncePad(new Rectangle(250, 270, 50, 15)));
+    portal.add(new Portal(new Rectangle(600, 440, 40, 60), 50, 220));
+    bottom = new Bottom(new Rectangle(150, 270, 80, 10)); 
+    openWall.add(new Floor(new Rectangle(500, 60, 10, 90)));
+    coins.add(new Coin(new Rectangle(330, 450, 20, 20)));
+    coins.add(new Coin(new Rectangle(480, 450, 20, 20)));
+    coins.add(new Coin(new Rectangle(100, 240, 20, 20)));
+    coins.add(new Coin(new Rectangle(600, 110, 20, 20)));
+}
 }
 
 
