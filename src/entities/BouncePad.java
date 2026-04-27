8794f754f29eb2a6c40b283c80aa723955037b08
MuadMSET <muad4404@gmail.com>
2026-04-27 04:04:53 +0100

    Update BouncePad.java
    added the variables and constructor

--- src/entities/BouncePad.java
+++ src/entities/BouncePad.java
@@ -1,5 +1,15 @@
 package entities;
 
+import java.awt.Rectangle;
+
 public class BouncePad {
+	
+    public Rectangle area;
+    public int bouncePower = -22; 
+
+    public BouncePad(Rectangle area) {
+        this.area = area;
+    }
+	
 
 }
