8458111dda12c7743714fd53ecb80c11dfe5293f
MuadMSET <muad4404@gmail.com>
2026-04-27 16:57:56 +0100

    Update BouncePadTest.java
    added methods for BouncePadTest.java

--- src/tests/BouncePadTest.java
+++ src/tests/BouncePadTest.java
@@ -1,5 +1,16 @@
 package tests;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.awt.Rectangle;
+
+import entities.*;
+
 public class BouncePadTest {
 
+    @Test
+    public void testBouncePadInitialization() {}
+
+    @Test
+    public void testSuccessfulBounce() {}
 }
