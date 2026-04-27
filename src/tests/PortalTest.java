3414f9c00a8baf5fdc2d8f613d675cc8cf6a8256
MuadMSET <muad4404@gmail.com>
2026-04-27 16:51:24 +0100

    Update PortalTest.java
    added the implementation for testPortalInitialization()

--- src/tests/PortalTest.java
+++ src/tests/PortalTest.java
@@ -9,7 +9,13 @@ import entities.*;
 class PortalTest {
 	
 	@Test
-	public void testPortalInitialization() {}
+	public void testPortalInitialization() {
+		// here we create a pprtal
+		Portal portal = new Portal(new Rectangle(0, 0, 50, 50), 500, 500);
+		// here we check if it was created or not
+		assertEquals(500, portal.X);
+        assertEquals(500, portal.Y);
+	}
 	@Test
 	public void testSuccessfulTeleport() {}
 	
