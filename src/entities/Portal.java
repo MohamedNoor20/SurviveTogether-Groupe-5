64789c44bb13064f77eca9e69e0bf1b548e966a8
MuadMSET <muad4404@gmail.com>
2026-04-27 03:25:36 +0100

    Update Portal.java
    added the variables and constructor for the portal

--- src/entities/Portal.java
+++ src/entities/Portal.java
@@ -1,5 +1,19 @@
 package entities;
 
+import java.awt.Rectangle;
+
 public class Portal {
+	
+	public Rectangle area;
+	public int X;
+	public int Y;
+	
+	public Portal (Rectangle area, int X, int Y) {
+		
+		this.area = area;
+		this.X = X;
+		this.Y = Y;
+	}
+	
 
 }
